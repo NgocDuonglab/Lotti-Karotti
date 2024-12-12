6697b5053e8cb0076860c43af7e34aa049c05491
kiwii <kimquynh15@gmail.com>
2024-12-12 15:12:21 +0100

    init

---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module com.example.lottikarotti_main {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens com.example.lottikarotti_main to javafx.fxml;
+    exports com.example.lottikarotti_main;
+}
