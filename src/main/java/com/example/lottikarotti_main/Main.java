89dfc3ae45078a70dba0c9a3293db43437a030ba
Bao Ngoc <baongoc29110405@gmail.com>
2024-12-15 02:41:14 +0100

    add and change entry point to match require type

---
+++ src/main/java/com/example/lottikarotti_main/Main.java
@@ -0,0 +1,26 @@
+package com.example.lottikarotti_main;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/example/lottikarotti_main/View/MainView.fxml"));
+        Parent root = loader.load();
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Lotti Karotti Main");
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
