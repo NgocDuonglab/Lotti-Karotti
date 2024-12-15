9e89accaf872883ac52ef0974ddcbf56290c30b0
Bao Ngoc <baongoc29110405@gmail.com>
2024-12-16 00:10:59 +0100

    styling main view

--- src/main/java/LottiKarottiGameHandler/Start/lottikarotti_main/GameController.java
+++ src/main/java/LottiKarottiGameHandler/Start/lottikarotti_main/GameController.java
@@ -2,65 +2,48 @@ package LottiKarottiGameHandler.Start.lottikarotti_main;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Rectangle;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class GameController implements Initializable {
 
     @FXML
-    private Label myLabel;
-
-    @FXML
-    private ChoiceBox<String> myChoiceBox;
-
-    @FXML
     private ImageView myImageView;
-
-    @FXML
-    private Rectangle myRectangle;
-
-    @FXML
-    private Button myButton;
-
-    private String[] number = {"2 Players", "3 Players", "4 Players"};
 
     @Override
     public void initialize(URL arg0, ResourceBundle arg1) {
-        myChoiceBox.getItems().addAll(number);
-        myChoiceBox.setOnAction(this::getChoice);
         setBackgroundImage();
-        setRectangleBackground();
-        myButton.setVisible(false);
     }
 
-    public void getChoice(ActionEvent event) {
-        String myChoice = myChoiceBox.getValue();
-        myLabel.setText(myChoice);
-        myLabel.setTextFill(Color.BLACK);
-        myLabel.setLayoutX((myRectangle.getLayoutX() + myRectangle.getWidth() / 2) - (myLabel.getWidth() / 2));
-        myLabel.setLayoutY(myRectangle.getLayoutY() + (myRectangle.getHeight() / 2) - (myLabel.getHeight() / 2));
-        myButton.setVisible(true);
-        myButton.setLayoutX((myImageView.getFitWidth() / 2) - (myButton.getWidth() / 2));
-        myButton.setLayoutY(myRectangle.getLayoutY() + myRectangle.getHeight() + 20);
-        myChoiceBox.setVisible(false);
+    @FXML
+    private void handleButtonAction(ActionEvent event) {
+        Button clickedButton = (Button) event.getSource();
+        String buttonText = clickedButton.getText();
+        System.out.println("Button clicked: " + buttonText);
+
+        // Load the next scene
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/LottiKarottiGameHandler/Start/lottikarotti_main/View/NextScene.fxml"));
+            Parent root = loader.load();
+            Stage stage = (Stage) clickedButton.getScene().getWindow();
+            stage.setScene(new Scene(root));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     private void setBackgroundImage() {
         Image backgroundImage = new Image(getClass().getResourceAsStream("/LottiKarottiGameHandler/Start/lottikarotti_main/images/main-background.png"));
         myImageView.setImage(backgroundImage);
-    }
-
-    private void setRectangleBackground() {
-        myRectangle.setFill(Color.WHITE);
-        myRectangle.setOpacity(0.7);
     }
 }
