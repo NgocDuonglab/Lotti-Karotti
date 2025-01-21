4d85164ea74bb0ce7bf074c261d07e1bef50b1ff
kiwii <kimquynh15@gmail.com>
2025-01-22 00:58:14 +0100

    add icon

--- src/main/java/GameHandler/lottikarotti_main/Game.java
+++ src/main/java/GameHandler/lottikarotti_main/Game.java
@@ -4,8 +4,10 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
+import javax.swing.*;
 import java.io.IOException;
 
 public class Game extends Application {
@@ -13,10 +15,17 @@ public class Game extends Application {
     @Override
     public void start(Stage primaryStage) throws IOException {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/GameHandler/lottikarotti_main/View/MainView.fxml"));
+
+
         Parent root = loader.load();
         Scene scene = new Scene(root);
         primaryStage.setScene(scene);
-        primaryStage.setTitle("Lotti Karotti Main");
+        primaryStage.setTitle("Lotti Karotti");
+
+        // Set the icon for the primary stage
+        Image icon = new Image(getClass().getResourceAsStream("/GameHandler/lottikarotti_main/images/carrot.png"));
+        primaryStage.getIcons().add(icon);
+
         primaryStage.show();
 
         SceneChanger.setPrimaryStage(primaryStage);
